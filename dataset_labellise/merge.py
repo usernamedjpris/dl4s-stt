5e6aff3d1bd886aa673bd51562700247915eff41
mk <collobert@juniorinsaservices.fr>
2021-06-24 10:53:43 +0200

    big

--- dataset_labellise/merge.py
+++ dataset_labellise/merge.py
@@ -259,7 +259,7 @@ def main(args):
     # convert_audios(args.cv)
 
     # Merge "physique" des fichiers audios dans le dossier de commonvoice
-    generate_dataset(pd.concat([wp1_test_sample,wp1_train_sample]), args.wp1, args.cv)
+    # generate_dataset(pd.concat([wp1_test_sample,wp1_train_sample]), args.wp1, args.cv)
 
     # Calculer la durée en fonction du split
     # calculate_duration(args.cv)
