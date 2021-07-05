5ecd75cd99ac5c3992b89cb88c676962f0b69a65
yamsok <collobert@juniorinsaservices.fr>
2021-07-05 17:49:23 +0200

    sqd

--- data_mining/gpu_segmenter.py
+++ data_mining/gpu_segmenter.py
@@ -53,7 +53,7 @@ def main(args):
     #     filename = os.path.join(input_dir, yt_id + ".wav")
     #     wav.append(filename)
     wav = pd.read_csv(args.split)['path'].apply(lambda x : os.path.join(args.clips, x)).to_list()
-    segmented = [f.split("/")[-1][:-4] for f in glob.glob(os.join(args.output_dir, "*.wav"))]
+    segmented = [f.split("/")[-1][:-4] for f in glob.glob(os.path.join(args.output_dir, "*.wav"))]
     print(segmented)
     wav = [w for w in real_wav if w in wav and w.split("/")[-1][:-4] not in segmented]
     segmentation_inaspeech(wav, args.output_dir, args.batch_size)
