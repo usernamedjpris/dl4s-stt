9feba5dd1e1f641aaaf8035c290e7a0c10c423cd
yamsok <collobert@juniorinsaservices.fr>
2021-07-08 15:00:21 +0200

    new method

--- data_mining/validate_segmentation.py
+++ data_mining/validate_segmentation.py
@@ -28,18 +28,30 @@ def get_real_duration(args):
 
     return sum(dataset["duration"]), len(wav)
 
-def get_real_duration(args):
-    cmd = 'for x in ' + os.path.join(args.clips, "*.wav") + \
-    '; do ffprobe -i $x -show_entries format=duration -v quiet -of csv="p=0"; done > ' + \
-    os.path.join(args.clips, "duration.txt")
+# def get_real_duration(args):
+#     cmd = 'for x in ' + os.path.join(args.clips, "*.wav") + \
+#     '; do ffprobe -i $x -show_entries format=duration -v quiet -of csv="p=0"; done > ' + \
+#     os.path.join(args.clips, "duration.txt")
 
-    print(cmd)
-    run(cmd, shell=True)
+#     print(cmd)
+#     run(cmd, shell=True)
     
-    with open(os.path.join(args.clips, "duration.txt"), 'r') as durations:
-        c = durations.read()
+#     with open(os.path.join(args.clips, "duration.txt"), 'r') as durations:
+#         c = durations.read()
     
-    return sum([float(x) for x in c.split("\n") if len(x) > 0])
+#     return sum([float(x) for x in c.split("\n") if len(x) > 0])
+
+def get_real_duration(args):
+
+    fnames = glob.glob(os.path.join(args.clips, "*.wav"))
+    with open(os.path.join(args.clips, "duration.csv"), 'r+') as durations:
+        print('path\tduration', file=durations)
+        for fname in tqdm(fnames):
+            cmd = 'ffprobe -i '+ fname +' -show_entries format=duration -v quiet -of csv="p=0"'
+            output  = run(cmd, shell=True, capture_output=True)
+            print(f"{fname}\t{output.stdout[:-2].decode('utf-8')}", file=durations) # -2 pour enlever "\n"
+
+    return os.path.join(args.clips, "duration.csv")
 
 def get_theoretical_duration(args):
 
@@ -70,8 +82,9 @@ def main(args):
 
 
     if args.clips:
-        duration = get_real_duration(args)
-        print(duration/3600)
+        durations = pd.read_csv(get_real_duration(args), '\t')
+        
+        # print(duration/3600)
         # print("Real duration :", duration / 3600, "h for", nb_files, "files")
 
 if __name__ == "__main__":    
